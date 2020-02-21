ff9e1f3ac1673ebf6b8b0d235270bedde5ba7001
jinwoong <jinwoongma@gmail.com>
2020-02-21 23:17:56 +0900

    add new file

--- 백준/test.py
+++ 백준/test.py
@@ -1,2 +1,9 @@
-a = list(map(int,input()))
-print(a)
+def fatorial(n):
+    if n == 0:
+        return 1
+    if n < 2:
+        return n
+    return n * fatorial(n-1)
+
+N = int(input())
+print(fatorial(N))
